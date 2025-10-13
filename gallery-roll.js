7afd741fe9f644146f6248a56214dd36a43774e3
Ondřej Marek <om@omarek.cz>
2025-10-13 19:22:50 +0200

    CZ logo, gallery fixes, new records

--- gallery-roll.js
+++ gallery-roll.js
@@ -1,69 +1,58 @@
-const gallerySections = document.querySelectorAll('.gallery-section');
-const photosGrids = document.querySelectorAll('.photos-grid');
+// max-height is set for each .photos-grid
+// grid is closed when .photos-grid--closed is applied
 
-const storeInitialHeights = () => {
-  // Opening
-  gallerySections.forEach(section => {
-    section.classList.remove('roll-closed');
-  });
-  // Storing heights of photo grids
-  photosGrids.forEach(grid => {
-    grid.dataset.initialHeight = grid.clientHeight;
-  });
-  // Default closing
-  gallerySections.forEach(section => {
-    section.classList.add('roll-closed');
-  });
-}
-storeInitialHeights();
+const grids = [...document.querySelectorAll('.photos-grid')];
 
-// Each time window resizes initial heights must be remeasured
-window.addEventListener('resize', storeInitialHeights);
+const closeAllGrids = () => {
+	for (const grid of grids) {
+		grid.classList.add('photos-grid--closed');
+	}
+};
+
+const refreshFullHeights = () => {
+	for (const grid of grids) {
+		const wasClosed = grid.classList.contains('photos-grid--closed');
+		grid.classList.remove('photos-grid--closed');
+		grid.style.maxHeight = `unset`;
+		grid.style.maxHeight = `${grid.clientHeight}px`;
+		if (wasClosed)
+			grid.classList.add('photos-grid--closed');
+	}
+};
 
 
-// Adding buttons
-gallerySections.forEach(section => {
-  // open
-  let btnOpen = document.createElement('a');
-  btnOpen.classList.add('roll-control', 'roll-btn-open');
-  btnOpen.innerHTML = 'Zobrazit více fotek <div style="display: flex; align-items: center; width: 1em; margin-left: 0.2em;"><svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg></div>';
 
-  // close
-  let btnClose = document.createElement('a');
-  if(section.id) {
-    // btnClose.href = '#' + section.id;
-  }
-  btnClose.classList.add('roll-control', 'roll-btn-close');
-  btnClose.innerHTML = 'Méně fotek <div style="display: flex; align-items: center; width: 1em; margin-left: 0.2em;"><svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 15l7-7 7 7"></path></svg></div>';
+const renderBtns = () => {
+	for(const grid of grids) {
+		const btnOpen = document.createElement('button');
+		const btnClose = document.createElement('button');
+		btnOpen.classList.add('roll-btn');
+		btnClose.classList.add('roll-btn', 'roll-btn--close');
+		btnOpen.innerHTML = 'Zobrazit více fotek <div style="display: flex; align-items: center; width: 1em; margin-left: 0.2em;"><svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg></div>';
+		btnClose.innerHTML = 'Méně fotek <div style="display: flex; align-items: center; width: 1em; margin-left: 0.2em;"><svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 15l7-7 7 7"></path></svg></div>';
+		btnOpen.addEventListener('click', () => grid.classList.remove('photos-grid--closed'));
+		btnClose.addEventListener('click', () => grid.classList.add('photos-grid--closed'));
+		grid.parentElement.appendChild(btnOpen);
+		grid.parentElement.appendChild(btnClose);
+	}
 
-  // adding
-  section.appendChild(btnOpen);
-  section.appendChild(btnClose);
-});
+};
 
-const rollOpenBtns = document.querySelectorAll('.roll-btn-open');
-const rollCloseBtns = document.querySelectorAll('.roll-btn-close');
+let refreshTimout = undefined;
+const initAutoRefreshAfterResize = () => {
+	const refreshDebounce_MS = 1300;
+	const debounceRefresh = () => {
+		clearTimeout(refreshTimout);
+		refreshTimout = setTimeout(refreshFullHeights, refreshDebounce_MS);
+	};
+	window.addEventListener('resize', debounceRefresh);
+};
 
-// Making buttons work
-rollOpenBtns.forEach(btn => {
-  btn.addEventListener('click', () => {
-    const photosGrid = btn.parentElement.querySelector('.photos-grid');
-    photosGrid.style.maxHeight = photosGrid.dataset.initialHeight + 'px';
-    // class for hiding right btns
-    photosGrid.parentElement.classList.remove('roll-closed');
-  });
-});
+const initGarellyRoll = () => {
+	renderBtns();
+	refreshFullHeights();
+	closeAllGrids();
+	initAutoRefreshAfterResize();
+};
 
-rollCloseBtns.forEach(btn => {
-  btn.addEventListener('click', () => {
-    const photosGrid = btn.parentElement.querySelector('.photos-grid');
-    photosGrid.style.maxHeight = '';
-    // class for hiding right btns
-    photosGrid.parentElement.classList.add('roll-closed');
-
-    // scrolling
-    const headerHeight = document.querySelector('header').clientHeight;
-    const sectionDistanceToTop = window.pageYOffset + photosGrid.parentElement.getBoundingClientRect().top;
-    window.scroll(0, sectionDistanceToTop - headerHeight);
-  });
-});
+initGarellyRoll();
